f7b9b862fca160b88339be10c34a3fd5fe1b11af
ZombieAlien <sanna.gustafsson0706@gmail.com>
2017-10-04 14:14:07 +0200

    Added dubble jump, wall jump, crouch and controller support

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    public PlayerController player;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<PlayerController>();
+    }
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Ground"))
+    //    {
+    //        player.grounded = true;
+    //    }
+    //}
+    //void OnTriggerStay2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Ground"))
+    //    {
+    //        player.grounded = true;
+    //    }
+    //}
+
+    //void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Ground"))
+    //    {
+    //        player.grounded = false;
+    //    }
+    //}
+}
--- SPG4 - S.A.D. Project/Assets/Scripts/PlayerController.cs
+++ SPG4 - S.A.D. Project/Assets/Scripts/PlayerController.cs
@@ -8,54 +8,109 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 3f;
     public float maxSpeed = 10f;
+    public float crouchSpeed = 2f;
     public float jumpForce = 5000f;
 
-    public bool jumpState;
-    public bool oldJumpState;
-    public bool grounded;
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
 
     public Vector2 position;
     public Rigidbody2D ridgidbodyPlayer;
 
-    private float h;
+    private bool jumpState;
+    private bool oldJumpState;
+    private bool grounded;
+    private bool hasDoubleJumped;
+    private bool isOnWall;
+    private bool crouchState;
 
+    private float horizontalInput;
     private Vector2 velocity;
 
     private Animator animator;
+
+    private GameObject defaultCollider;
+    private GameObject crouchCollider;
 
     // Use this for initialization
     void Start ()
     {
         ridgidbodyPlayer = gameObject.GetComponent<Rigidbody2D>();
         animator = gameObject.GetComponent<Animator>();
-	}
+
+        defaultCollider = transform.Find("Default Collider").gameObject;
+        crouchCollider = transform.Find("Crouch Collider").gameObject;
+
+        crouchCollider.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (grounded)
+            hasDoubleJumped = false;
+
         position.x = transform.position.x;
         position.y = transform.position.y;
+
+        if (crouchState)
+        {
+            defaultCollider.SetActive(false);
+            crouchCollider.SetActive(true);
+        }
+        if (!crouchState)
+        {
+            defaultCollider.SetActive(true);
+            crouchCollider.SetActive(false);
+        }
     }
 
     private void FixedUpdate()
     {
-        Movement();
-        SpeedLimit();
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        isOnWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
 
         oldJumpState = jumpState;
         jumpState = Input.GetButton("Jump" + playerNumber);
+        crouchState = Input.GetButton("Crouch" + playerNumber);
 
-        if (jumpState)
+        Movement();
+        SpeedLimit();
+        TurnToInputDirection();
+
+        if (jumpState && !oldJumpState && grounded)
         {
             Jump();
+        }
+
+        if (jumpState && !oldJumpState && !grounded && !hasDoubleJumped)
+        {
+            Jump();
+            hasDoubleJumped = true;
+        }
+
+        if (isOnWall)
+        {
+            grounded = false;
+            hasDoubleJumped = false;
         }
     }
 
     private void Movement()
     {
-        h = Input.GetAxis("Horizontal" + playerNumber);
+        horizontalInput = Input.GetAxis("Horizontal" + playerNumber);
 
-        velocity.x = (speed * h);
+        if (crouchState)
+            velocity.x = (crouchSpeed * horizontalInput);
+
+        else
+            velocity.x = (speed * horizontalInput);
+
         velocity.y = this.ridgidbodyPlayer.velocity.y;
 
         this.ridgidbodyPlayer.velocity = velocity;
@@ -63,13 +118,11 @@ public class PlayerController : MonoBehaviour {
 
     public void Jump()
     {
-        if (jumpState && !oldJumpState)
-        {
-            ridgidbodyPlayer.velocity = new Vector2(ridgidbodyPlayer.velocity.x, 0);
-            velocity = ridgidbodyPlayer.velocity;
-            ridgidbodyPlayer.AddForce(Vector2.up * jumpForce);
-        }
+        ridgidbodyPlayer.velocity = new Vector2(ridgidbodyPlayer.velocity.x, 0);
+        velocity = ridgidbodyPlayer.velocity;
+        ridgidbodyPlayer.AddForce(Vector2.up * jumpForce);
     }
+
 
     private void SpeedLimit()
     {
@@ -81,6 +134,19 @@ public class PlayerController : MonoBehaviour {
         else if (ridgidbodyPlayer.velocity.x < -maxSpeed)
         {
             ridgidbodyPlayer.velocity = new Vector2(-maxSpeed, ridgidbodyPlayer.velocity.y);
+        }
+    }
+
+    private void TurnToInputDirection()
+    {
+        if (Input.GetAxis("Horizontal" + playerNumber) < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetAxis("Horizontal" + playerNumber) > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
         }
     }
 }
