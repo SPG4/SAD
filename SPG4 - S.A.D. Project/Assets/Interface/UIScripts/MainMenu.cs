7b809461e8559851dd4cff21ec5ffcccccf37b26
Ghostiex <joel.o.l.nilsson@gmail.com>
2017-10-16 13:49:56 +0200

    Main Menu Added
    Main Menu added, with ability to change music volume and sfx volume.
    These are not connected to game yet. New game starts lvl 1, select level
    will show unlocked levels. Load Game not implemented.

---
+++ SPG4 - S.A.D. Project/Assets/Interface/UIScripts/MainMenu.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Animator CameraObject;
+    public GameObject PanelGame;
+    public AudioSource hoverSound;
+    public AudioSource sfxhoversound;
+    public AudioSource clickSound;
+    public GameObject areYouSure;
+
+    // Campaign button sub menu
+    public GameObject continueBtn;
+    public GameObject newGameBtn;
+    public GameObject loadGameBtn;
+
+    // Level Menu Buttons
+    public GameObject levelOneBtn;
+
+    // Highlights
+    public GameObject lineGame;
+    public GameObject lineVideo;
+    public GameObject lineControls;
+    public GameObject lineKeyBindings;
+    public GameObject lineMovement;
+    public GameObject lineCombat;
+    public GameObject lineGeneral;
+
+    public void PlayCampaign()
+    {
+        areYouSure.gameObject.SetActive(false);
+        continueBtn.gameObject.SetActive(true);
+        newGameBtn.gameObject.SetActive(true);
+        loadGameBtn.gameObject.SetActive(true);
+    }
+	public void DisablePlayCampaign()
+    {
+        continueBtn.gameObject.SetActive(false);
+        newGameBtn.gameObject.SetActive(false);
+        loadGameBtn.gameObject.SetActive(false);
+        DisableSelectLevel();
+    }
+
+    public void SelectLevel()
+    {
+        levelOneBtn.gameObject.SetActive(true);
+    }
+
+    public void DisableSelectLevel()
+    {
+        levelOneBtn.gameObject.SetActive(false);
+    }
+
+    public void Position2()
+    {
+        DisablePlayCampaign();
+        CameraObject.SetFloat("Animate", 1);
+    }
+
+    public void Position1()
+    {
+        CameraObject.SetFloat("Animate", 0);
+    }
+
+    public void GamePanel()
+    {
+        PanelGame.gameObject.SetActive(true);
+
+        lineGame.gameObject.SetActive(true);
+        lineControls.gameObject.SetActive(false);
+        lineVideo.gameObject.SetActive(false);
+        lineKeyBindings.gameObject.SetActive(false);
+    }
+
+    public void PlayHover()
+    {
+        hoverSound.GetComponent<AudioSource>();
+        hoverSound.Play();
+    }
+    public void PlaySFXHover()
+    {
+        sfxhoversound.GetComponent<AudioSource>();
+        sfxhoversound.Play();
+    }
+
+    public void PlayClick()
+    {
+        clickSound.GetComponent<AudioSource>();
+        clickSound.Play();
+    }
+
+    public void AreYouSure()
+    {
+        areYouSure.gameObject.SetActive(true);
+        DisablePlayCampaign();
+    }
+
+    public void No()
+    {
+        areYouSure.gameObject.SetActive(false);
+    }
+
+    public void Yes()
+    {
+        Application.Quit();
+    }
+}
---
+++ SPG4 - S.A.D. Project/Assets/Interface/UIScripts/OptionsMenu.js
@@ -0,0 +1,86 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+// toggle buttons
+var fullscreentext : GameObject;
+var tooltipstext : GameObject;
+
+// sliders
+var musicSlider : GameObject;
+var sfxSlider : GameObject;
+
+private var sliderValue : float = 0.0;
+private var sliderValueSFX : float = 0.0;
+
+function Start () {
+    // check difficulty
+    //if(PlayerPrefs.GetInt("NormalDifficulty") == 1){
+    //    //difficultynormaltext.GetComponent (Text).text = "NORMAL";
+    //    difficultynormaltextLINE.gameObject.active = true;
+    //    difficultyhardcoretextLINE.gameObject.active = false;
+    //    //difficultyhardcoretext.GetComponent (Text).text = "hardcore";
+    //}
+    //else
+    //{
+    //    //difficultynormaltext.GetComponent (Text).text = "normal";
+    //    //difficultyhardcoretext.GetComponent (Text).text = "HARDCORE";
+    //    difficultyhardcoretextLINE.gameObject.active = true;
+    //    difficultynormaltextLINE.gameObject.active = false;
+
+    }
+
+    // check slider values
+    musicSlider.GetComponent.<Slider>().value = PlayerPrefs.GetFloat("MusicVolume");
+    sfxSlider.GetComponent.<Slider>().value = PlayerPrefs.GetFloat("SFXVolume");
+
+    //// check full screen
+    //if(Screen.fullScreen == true){
+    //    fullscreentext.GetComponent (Text).text = "on";
+    //}
+    //else if(Screen.fullScreen == false){
+    //    fullscreentext.GetComponent (Text).text = "off";
+    //}
+
+    //// check hud value
+    //if(PlayerPrefs.GetInt("ShowHUD")==0){
+    //    showhudtext.GetComponent (Text).text = "off";
+    //}
+    //else{
+    //    showhudtext.GetComponent (Text).text = "on";
+    //}
+
+    // check tool tip value
+    if(PlayerPrefs.GetInt("ToolTips")==0){
+        tooltipstext.GetComponent (Text).text = "off";
+    }
+    else{
+        tooltipstext.GetComponent (Text).text = "on";
+    }
+
+
+function Update(){
+	sliderValue = musicSlider.GetComponent.<Slider>().value;
+	sliderValueSFX = sfxSlider.GetComponent.<Slider>().value;
+}
+
+
+function MusicSlider(){
+	PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+}
+
+function SFXSlider(){
+	PlayerPrefs.SetFloat("SFXVolume", sliderValueSFX);
+}
+
+
+// show tool tips like: 'How to Play' control pop ups
+function ToolTips(){
+	if(PlayerPrefs.GetInt("ToolTips")==0){
+		PlayerPrefs.SetInt("ToolTips",1);
+		tooltipstext.GetComponent (Text).text = "on";
+	}
+	else if(PlayerPrefs.GetInt("ToolTips")==1){
+		PlayerPrefs.SetInt("ToolTips",0);
+		tooltipstext.GetComponent (Text).text = "off";
+	}
+}
