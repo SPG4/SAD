b5f62d7273d38bcc35ff783110212a7ea7836200
Ghostiex <joel.o.l.nilsson@gmail.com>
2017-10-17 01:17:24 +0200

    Adding AbilityArea. New Scene. Prefabs from players.
    New script for AbilityArea. lots of TODO in it.

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/AbilityArea.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityArea : MonoBehaviour {
+
+    bool player1Ready, player2Ready, playersReady;
+    string currentAbility;
+    GameObject abilityArea;
+
+	// Use this for initialization
+	void Start () {
+        CheckLatestUsedAbility();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (!playersReady)
+        {
+            CheckPlayerInput(1);
+            CheckPlayerInput(2);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+    }
+
+    void CheckPlayerInput(int p)
+    {
+        if (Input.GetAxis("Vertical"+p) == 1)
+        {
+            //Call animation "down"
+            //Set current ability
+        }
+        else if (Input.GetAxis("Vertical"+p) == -1)
+        {
+            //Call animation "up"
+            //Set current ability
+        }
+        else if (Input.GetAxis("Submit") == 1)
+        {
+            //Remove boxes (add green checkbox?)
+            //Set current ability
+            if (p == 1)
+            {
+                gameObject.SendMessage(currentAbility);
+                player1Ready = true;
+            }
+            else
+                player2Ready = true;
+        }
+        
+    }
+
+    void CheckLatestUsedAbility()
+    {
+        //TODO set current ability to last used (before death for example)
+    }
+
+    void LoadAbilities()
+    {
+        //TODO load all existing abilities?????????????
+    }
+
+    void ReadyCheck()
+    {
+        if (player1Ready && player2Ready)
+        {
+            playersReady = true;
+        }
+    }
+}
