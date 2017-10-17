845650d1e8981d0e8e9518207bcdc988d9f92aed
ThatZombieAlien <sanna.gustafsson0706@gmail.com>
2017-10-17 17:19:03 +0200

    Shield Deflection
    Player 1 can now deflect projectiles with the shield

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/DeflectingShield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeflectingShield : MonoBehaviour {
+
+    /// <summary>
+    /// Checks collision and if the object should "bounce" off the shield it will be given a new velocity
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 13)
+        {
+            Vector3 newVelocity;
+            if (this.transform.parent.gameObject.transform.localScale.x == 1 && other.gameObject.GetComponent<Rigidbody2D>().velocity.x < 0 ||
+                this.transform.parent.gameObject.transform.localScale.x == -1 && other.gameObject.GetComponent<Rigidbody2D>().velocity.x > 0)
+            {
+                newVelocity = Vector3.Reflect(other.gameObject.GetComponent<Rigidbody2D>().velocity / 5, Vector3.right); //fix variable for speed? is set in "Turret" as "speedFactor"
+                other.gameObject.GetComponent<Rigidbody2D>().velocity = newVelocity * 5;
+            }
+        }
+    }
+}
+
