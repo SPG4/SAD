5bf3e0152040ce10fd2f52ce110b4e5decc493be
ShervinMansouri <shervin05@hotmail.com>
2017-10-22 17:39:06 +0200

    Cutscene
    La till cutscene i början av lvl 1

---
+++ SPG4 - S.A.D. Project/Assets/Animations/CameraSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public Camera cameraMain;
+    public Camera camera2;
+    public float time = 0.0f;
+	// Use this for initialization
+	void Start () {
+        cameraMain.enabled = false;
+        camera2.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+
+		    if (time >= 5.0f)
+        {
+            
+            //camera.enabled = !camera.enabled;
+            camera2.enabled = false; /* = !camera2.enabled;*/
+            cameraMain.enabled = true;
+        }
+	}
+}
