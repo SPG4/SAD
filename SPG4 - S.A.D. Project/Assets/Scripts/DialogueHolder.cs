eac4b1e7383f36224fd135e0a53a52948ee1e20d
ShervinMansouri <shervin05@hotmail.com>
2017-10-25 13:59:00 +0200

    Dialog box

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager diaManager;
+
+    // Use this for initialization
+    void Start () {
+        diaManager = FindObjectOfType<DialogueManager>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+                diaManager.ShowBox(dialogue);            
+        }
+    }
+}
---
+++ SPG4 - S.A.D. Project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject diaBox;
+    public Text diaText;
+    public bool diaActive;
+    public string[] diaLines;
+    public int currentLine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (diaActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            // diaBox.SetActive(false);
+            //diaActive = false;
+            currentLine++;
+        }
+
+        if (currentLine >= diaLines.Length)
+        {
+            diaBox.SetActive(false);
+            diaActive = false;
+            currentLine = 0;
+        }
+        diaText.text = diaLines[currentLine];
+    }
+    
+    public void ShowBox(string dialogue)
+    {
+        diaActive = true;
+        diaBox.SetActive(true);
+        diaText.text = dialogue;
+    }
+}
