8dd7a087f44698daf6da299b3609e05f7a98d4c9
annaliljaenglund <annaliljaenglund@gmail.com>
2017-11-05 11:48:53 +0100

    Level 3 update
    - Added content to level 3 (puzzles not quite finished)

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Level 3/VolumeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+
+        //turn down music
+    }
+}
