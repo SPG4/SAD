1bb1bd20ec08bdf81e6f2449a9709fb4bbaf1aa4
Ghostiex <joel.o.l.nilsson@gmail.com>
2017-11-15 20:10:00 +0100

    addedassets

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Globals/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+    public static GameControl instance;
+    public float scrollSpeed = -1.5f;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
