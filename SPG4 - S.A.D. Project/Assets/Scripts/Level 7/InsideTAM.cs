4508cd75712ef4e0c6818845c58ca594486d1225
MaximilianAlmo <maximilian.almo@gmail.com>
2017-11-24 13:17:13 +0100

    Level 7
    Started working on lvl 7

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Level 7/InsideTAM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideTAM : MonoBehaviour {
+
+
+    float time;
+	// Use this for initialization
+	void Start () {
+        time = 40;
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            // transport players out of area. 
+            Start();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
