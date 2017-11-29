bf389a2954027e896979c82e71429b15f4f915d5
ShervinMansouri <shervin05@hotmail.com>
2017-11-29 20:32:47 +0100

    Level 7 spike puzzle script

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Level 7/FallingSpikes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour {
+    public float spikeTime;
+    public bool falling;
+    public bool fallTime;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (fallTime == true)
+        {
+            spikeTime += Time.deltaTime;
+        }
+        if (spikeTime >= 10.0f)
+        {
+            falling = true;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Boss"))
+        {
+            
+            fallTime = true;
+        }
+    }
+}
