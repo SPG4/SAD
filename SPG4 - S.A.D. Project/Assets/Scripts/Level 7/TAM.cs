6f1fef34050e7deccc0f2a2ac99240edceb3ecfc
annaliljaenglund <annaliljaenglund@gmail.com>
2017-12-01 14:40:31 +0100

    Level 3 fixes + TAM-script update
    - Added textures for level 3
    - Changed the TAM-script, TAM can now take damage

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Level 7/TAM.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAM : MonoBehaviour
+{
+    bool hasPaused = true;
+    float timer;
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    float speed = 0.1f;
+    private float direction = 1.0f;
+    private float currentPosition = 0.0f;
+
+    public float health = 3;
+    float damage;
+
+    void Update()
+    {
+        TAMMovement();
+    }
+
+    /// <summary>
+    /// Controls the movement of TAM
+    /// </summary>
+    void TAMMovement()
+    {
+        currentPosition = Mathf.Clamp01(currentPosition + speed * Time.deltaTime * direction);
+
+        //Changes directions of TAM if currenPosition has either reached 0 or 1
+        if (direction == 1.0f && currentPosition > 0.99f)
+            direction = -1.0f;
+        if (direction == -1.0f && currentPosition < 0.01f)
+            direction = 1.0f;
+
+        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, currentPosition);
+
+        if (currentPosition >= 0.99f)
+            hasPaused = false;
+
+        //If TAM has either reached the startpoint or endpoint, the pause-couroutine will start
+        if (currentPosition >= 0.99f && !hasPaused || currentPosition <= 0.01f && !hasPaused)
+        {
+            StartCoroutine(PauseMovement());
+            hasPaused = true;
+        }
+    }
+
+    public void ApplyDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    /// <summary>
+    /// Changes TAM's speed for two seconds
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator PauseMovement()
+    {
+        speed = 0;
+        yield return new WaitForSeconds(2f);
+        speed = 0.1f;
+        hasPaused = false;
+    }
+}
