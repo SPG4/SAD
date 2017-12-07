26921806f7a55d627e5cb37af99b8a9a56c58591
ShervinMansouri <shervin05@hotmail.com>
2017-12-07 21:01:05 +0100

    Level 5 Sound + Texture

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Level 5/Lvl5EyeSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl5EyeSound : MonoBehaviour {
+    public AudioClip playSound;
+    public float volume;
+    private AudioSource audioSource;
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Eye"))
+        {
+            audioSource.PlayOneShot(playSound, 0.7f);
+        }
+    }
+}
