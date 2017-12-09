fb88bcbe0f5a1e9ce86d8ebc29af71d627281d73
Ghostiex <joel.o.l.nilsson@gmail.com>
2017-12-09 22:52:50 +0100

    Added numpad enter loads next scene

---
+++ SPG4 - S.A.D. Project/Assets/Scripts/Globals/LoadScenesForShow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScenesForShow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene + 1, LoadSceneMode.Single);
+    }
+}
