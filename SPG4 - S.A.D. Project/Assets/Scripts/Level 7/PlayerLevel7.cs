b5b286cea1af369d688f34b307e3b7cba8852d36
Sanna Gustafsson <sanna.gustafsson0706@gmail.com>
2017-12-10 19:33:03 +0100

    Dialogue Manager changes & added Game Handler to scenes

--- SPG4 - S.A.D. Project/Assets/Scripts/Level 7/PlayerLevel7.cs
+++ SPG4 - S.A.D. Project/Assets/Scripts/Level 7/PlayerLevel7.cs
@@ -8,6 +8,8 @@ public class PlayerLevel7 : MonoBehaviour {
     public int player;
     GameObject camera;
     GameObject dialogue;
+    GameObject gameHandler;
+    int balanceOffset = 10;
 
     void Start ()
     {
@@ -25,24 +27,27 @@ public class PlayerLevel7 : MonoBehaviour {
             gameObject.GetComponent<Transform>().position -= new Vector3(3, 0, 0);
             camera = GameObject.FindGameObjectWithTag("MainCamera");
             camera.GetComponent<CameraController>().minSizeY = 7;
+            gameHandler = GameObject.FindGameObjectWithTag("GameHandler");
 
-            //if (balance){ 
-            //dialogue.SendMessage("AddText", "Green: Hey, we actually did it! Together!");
-            //dialogue.SendMessage("AddText", "Blue: Yeah, would you look at that, we’re a pretty good team! Maybe if we try to work together more from now on, things will get easier in the future!"); }
+            if (gameHandler.GetComponent<Save>().GetTotalPlayerPoint(1) >= gameHandler.GetComponent<Save>().GetTotalPlayerPoint(2) + balanceOffset) // blue bigger
+            {
+                dialogue.SendMessage("AddText", "Green: Hey, we did it!");
+                dialogue.SendMessage("AddText", "Blue: You mean I did it? This whole time you’ve just been along for the ride while I’ve been doing the real work. I think it’s time I get rid of the weak link.");
+                dialogue.SendMessage("AddText", "Green: Hey, what do you mean? What are you doing?");
+            }
 
-            //if (blue)
-            //{
-            //    dialogue.SendMessage("AddText", "Green: Hey, we did it!");
-            //    dialogue.SendMessage("AddText", "Blue: You mean I did it? This whole time you’ve just been along for the ride while I’ve been doing the real work. I think it’s time I get rid of the weak link.");
-            //    dialogue.SendMessage("AddText", "Green: Hey, what do you mean? What are you doing?");
-            //}
+            else if (gameHandler.GetComponent<Save>().GetTotalPlayerPoint(2) >= gameHandler.GetComponent<Save>().GetTotalPlayerPoint(1) + balanceOffset) //green bigger
+            {
+                dialogue.SendMessage("AddText", "Blue: Hey, we did it!");
+                dialogue.SendMessage("AddText", "Green: You mean I did it. This whole time you’ve just been undermining me, telling me I’m not good enough, but in reality I’m the only one doing the actual work! I’m sick of it, I’m sick of you!");
+                dialogue.SendMessage("AddText", "Green: Hey, no need overreact man! Wait, what are you doing?");
+            }
 
-            //if (green)
-            //{
-            //    dialogue.SendMessage("AddText", "Blue: Hey, we did it!");
-            //    dialogue.SendMessage("AddText", "Green: You mean I did it. This whole time you’ve just been undermining me, telling me I’m not good enough, but in reality I’m the only one doing the actual work! I’m sick of it, I’m sick of you!");
-            //    dialogue.SendMessage("AddText", "Green: Hey, no need overreact man! Wait, what are you doing?");
-            //}
+            else // balance
+            {
+                dialogue.SendMessage("AddText", "Green: Hey, we actually did it! Together!");
+                dialogue.SendMessage("AddText", "Blue: Yeah, would you look at that, we’re a pretty good team! Maybe if we try to work together more from now on, things will get easier in the future!");
+            }
         }
     }
 }
